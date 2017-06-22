90559d844a746ee0ee4ae096474ae92105c6b41e
Sarvesh Sirmukaddam <sarvesh.sirmukaddam@gmail.com>
2017-06-22 17:54:11 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,1 @@
-#include <arduino.h>
-#include <FreeRTOS.h>
-#include <Arduino.h>
-int ledpin=13;
-#define CAL_FACTOR(F_CPU/6007)
-void LEDTask(void *args)
-{
-  pinmode(ledpin, OUTPUT);
-  int delay1 = 500/portTICK_PERIOD_MS
-  for(;;)
-  {
-     digitalWrite(ledpin, LOW);
-     vTaskDelay(delay1);
-     digitalWrite(ledPin,High);
-     vTaskDelay(delay1);
-  }
-}
-     
+
